cb8d9995710c1b02488a6a7f916860fe30abdb31
Pedro Henriques <pedrohenriques@id.uff.br>
2026-08-26 19:54:30 -0300

    Remove OPCODE enum definition from simulator.c
    Removed the OPCODE enum definition for opcodes.

--- simulator/simulator.c
+++ simulator/simulator.c
@@ -29,22 +29,4 @@ void mic1_init(MIC1 *m) {
 }
 
 
-//facilitar a visu de opcodes (nada certo)
-typedef enum {
-    OP_LODD = 0b0000,
-    OP_STOD = 0b0001,
-    OP_ADDD = 0b0010,
-    // 0b0011 reservado pra instruções estendidas
-    OP_JPOS = 0b0100,
-    OP_JZER = 0b0101,
-    OP_JUMP = 0b0110,
-    OP_LOCO = 0b0111,
-    OP_LODL = 0b1000,
-    OP_STOL = 0b1001,
-    OP_ADDL = 0b1010,
-    OP_SUBL = 0b1011,
-    OP_JNEG = 0b1100,
-    OP_JNZE = 0b1101,
-    OP_CALL = 0b1110,
-    OP_EXTENDED = 0b1111 
-} OPCODE;
+
