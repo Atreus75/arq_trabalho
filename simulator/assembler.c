0f0e0390363172c95131f73f1dd0575f062dfc1d
Atreus75 <rodbr75@gmail.com>
2026-08-26 16:56:49 -0300

    Adicionado o tratamento de argumentos de linha de comando.

--- simulator/assembler.c
+++ simulator/assembler.c
@@ -3,9 +3,12 @@
 Assembly em texto ---> binário 01
 */
 #include "assembler.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 
-
-mem_instruction MEM_INSTRUCTION_ARRAY[MEM_INSTRUCTIONS] = {
+memInstruction memInstructionArray[MEM_INSTRUCTIONS] = {
 	{0b0000, "lodd"},
 	{0b0001, "stod"},
 	{0b0010, "addd"},
@@ -22,7 +25,7 @@ mem_instruction MEM_INSTRUCTION_ARRAY[MEM_INSTRUCTIONS] = {
 	{0b1110, "call"}
 };
 
-extended_instruction EXTENDED_INSTRUCTION_ARRAY[EXTENDED_INSTRUCTIONS] = {
+extendedInstruction extendedInstructionArray[EXTENDED_INSTRUCTIONS] = {
 	{0b1111, "push"},
 	{0b1111001, "popi"},
 	{0b111101, "push"},
@@ -34,6 +37,50 @@ extended_instruction EXTENDED_INSTRUCTION_ARRAY[EXTENDED_INSTRUCTIONS] = {
 	{0b11111111, "halt"}
 };
 
-int main(){
-	return 0; //coloquei isso aqui pro GCC não reclamar
+errorStruct errorCodes = {
+	.ArgumentError = 1
+};
+
+void raiseError(int code, int msgCount, ...){
+	va_list msgs;
+	va_start(msgs, msgCount);
+	for (int c = 0; c<msgCount; c++){
+		fprintf(stderr, va_arg(msgs, char *));
+	}
+	va_end(msgs);
+	exit(code);
+}
+
+void processArguments(int argc, char * argv[], int * optionStates, char * outputPath){
+// Argument processing function.
+	char * usageMessage = {
+		"usage: ./assembler [options]\n"
+		"\t-o: Optional file output path. Ex: ./assembler -o output.bin\n"
+		"\t-i: Interface method. Generates the binary content and sends for the web interface back-end. Use only with the web server interface.\n"
+	};
+
+	// Argument counting
+	if (argc < 2) raiseError(errorCodes.ArgumentError, 2, "[-] Argument error: no argument provided.\n\n", usageMessage);	
+	else if (argc > 4) raiseError(errorCodes.ArgumentError, 2, "[-] Argument error: bad argument sintax.\n\n", usageMessage);
+
+	// argument parsing
+	for (int c = 1; c < argc; c++){
+		if (strcmp(argv[c], "-o") == 0 && !optionStates[0]){
+			optionStates[0] = 1;
+			if (argc <= (c+1) || strcmp(argv[c+1], "-i") == 0){
+				raiseError(errorCodes.ArgumentError, 2, "[-] Argument error: no path provided for output.\n\n", usageMessage);
+			}
+			strcpy(outputPath, argv[++c]);
+		}else if (strcmp(argv[c], "-i") == 0 && !optionStates[1]){
+			optionStates[1] = 1;
+		}
+	}
+}
+
+int main(int argc, char * argv[]){
+	// -o = 0 | -i = 1
+	int optionStates[2] = {0, 0};
+	char outputPath[255] = {0};
+	processArguments(argc, argv, optionStates, outputPath);
+	return 0;
 }
--- simulator/assembler.h
+++ simulator/assembler.h
@@ -3,10 +3,19 @@
 #define MEM_INSTRUCTIONS 14
 
 // TIPOS
+
 typedef struct {
 	int opcode;
 	char name[5];
-} mem_instruction;
+} memInstruction;
 
-typedef mem_instruction extended_instruction;
+typedef memInstruction extendedInstruction;
+
+typedef struct {
+	int ArgumentError; 
+} errorStruct;
+
+// FUNÇÕES 
+void raiseError(int code, int msgCount, ...);
+void processArguments(int argc, char * argv[], int * optionStates, char * outputPath);
 
